2985409c6beaa59c8aaca749e3905d9144547667
brandonandre <andr0284@algonquinlive.com>
2019-10-01 12:40:28 -0400

    Added new move distance function and fixed some issues with the tight turning function.

--- src/movement.cpp
+++ src/movement.cpp
@@ -101,18 +101,51 @@ void moveDistance(long mm, int motorSpeed = SPEED, int direction = FORWARDS)
 }
 
 /*
+*    Purpose:       Move a given direction for a given number of milliseconds
+*    Date:          08/02/2019
+*    Authors:       Michael McRae / Brandon Andre
+*    Parameters:    Int representing milliseconds, Int representing direction 
+                    (1 FORWARDS, 2 BACKWARDS)
+*    Return value:  none
+*/
+void moveSeconds(int seconds, int direction)
+{
+    switch (direction) {
+    case FORWARDS:
+        mc.setMotorPowers(DC_ADDRESS, SPEED / 4, -SPEED / 4);
+        break;
+    case BACKWARDS:
+        mc.setMotorPowers(DC_ADDRESS, -SPEED / 4, SPEED / 4);
+        break;
+    default:
+        // TODO: Error message here...
+        return void;
+    }
+
+    // Wait for the specified amount of seconds.
+    Utils::waitFor(seconds);
+
+    // Stop the motor
+    mc.setMotorPowers(DC_ADDRESS, 0, 0);
+}
+
+/*
  *    Purpose: Allows both motors to turn
  *    Date: Septemeber 30, 2019
  *    Authors: Chris Ikongo
  *    Parameters: degrees(long) a negative number will cause it to turn to the left
  *                  and a positive number to the right
  *    Return value: N/A
- *     Algorithm: 2 * (pi) * (radius) for the radius and simplified circle math.
+ *    Algorithm: 2 * (pi) * (radius) for the radius and simplified circle math.
  */
 void tightTurn(long degrees)
 {
     // simplified math for distance each wheel should turn for specified degrees
-    long wheelTurn = (M_PI * JEFF_WIDTH * - degrees) / WHEEL_CIRC;
+    long wheelTurn = (M_PI * JEFF_WIDTH * -degrees) / WHEEL_CIRC;
 
-    mc.setMotorDegrees(DC_ADDRESS, motorSpeed, motorOneDegrees, motorSpeed, motorTwoDegrees);
+    mc.setMotorDegrees(DC_ADDRESS, SPEED, wheelTurn, SPEED, wheelTurn);
+
+    // Reset the encoders after the turn has taken place.
+    Utils::waitFor(wheelTurn * 15);
+    mc.resetEncoders();
 }
