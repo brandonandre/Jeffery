bf641cadf265a8de1330bf9b13a1c7aabccae432
brandonandre <andr0284@algonquinlive.com>
2019-10-03 20:49:00 -0400

    fixed conflict

--- src/robot.h
+++ src/robot.h
@@ -35,13 +35,10 @@ using namespace std;
 #define HEAD_SERVO 0x28 /* Address for the head servo 1 */
 #define CLAW_SERVO 0x29 /* Address for claw servo 2 */
 
-<<<<<<< HEAD
 /* Claw Constants */
 #define CLAW_OPEN 180
 #define CLAW_CLOSED 0
 
-=======
->>>>>>> e389ad2073db5396996610a0f18437abcdaf3dfe
 /* Function Prototypes */
 void begin();
 void moveDistance(long mm, int motorSpeed = SPEED, int direction = FORWARDS);
