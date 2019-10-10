2705bc8e4b7ad58058edd706fed51e45a79ce94f
brandonandre <andr0284@algonquinlive.com>
2019-10-10 13:52:57 -0400

    Added left and right turning...

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,31 @@ int main(int argc, char const *argv[])
         printf("Enter a command:\n");
         printf("\t1. Move Forwards\n");
         printf("\t2. Move Backwards\n");
-        printf("\t3. Tight Turn\n");
-        printf("\t4. Exit\n");
+        printf("\t3. Tight Turn Left\n");
+        printf("\t4. Tight Turn Right\n");
+        printf("\t5. Exit\n");
 
         int response;
         scanf("%d", &response);
 
         switch (response) {
         case 1:
-            moveDistance(20, SPEED, FORWARDS);
+            printf("MOVE FORWARDS!\n");
+            moveDistance(50, SPEED, FORWARDS);
             break;
         case 2:
-            moveDistance(20, SPEED, BACKWARDS);
+            printf("MOVE BACKWARDS!\n");
+            moveDistance(50, SPEED, BACKWARDS);
             break;
         case 3:
+            printf("TIGHT TURN LEFT!\n");
+            tightTurn(-20);
             break;
         case 4:
+            printf("TIGHT TURN RIGHT!\n");
+            tightTurn(20);
+            break;
+        case 5:
             running = false;
             break;
         
