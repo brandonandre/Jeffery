2c3a14e2cded424eb8b356773c1c47459b96c486
brandonandre <andr0284@algonquinlive.com>
2019-10-10 13:04:32 -0400

    Adding a running loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,37 @@ int main(int argc, char const *argv[])
 {
     begin();
 
-    moveDistance(100, SPEED, FORWARDS);
-    
+    bool running = true;
+    while(running) {
+        printf("Jeffery Control Panel\n");
+        printf("Enter a command:\n");
+        printf("\t1. Move Forwards\n");
+        printf("\t2. Move Backwards\n");
+        printf("\t3. Tight Turn\n");
+        printf("\t4. Exit\n");
+
+        int response;
+        scanf("%d", &response);
+
+        switch (response) {
+        case 1:
+            moveDistance(20, SPEED, FORWARDS);
+            break;
+        case 2:
+            moveDistance(20, SPEED, BACKWARDS);
+            break;
+        case 3:
+            break;
+        case 4:
+            running = false;
+            break;
+        
+        default:
+            printf("\nWrong command...\n");
+            break;
+        }
+    }
+
     close();
     return 0;
 }
