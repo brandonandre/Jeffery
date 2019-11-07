33d8e40cb52ec6389c23bde836160220ab579b35
brandonandre <andr0284@algonquinlive.com>
2019-11-07 15:53:15 -0500

    version 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include "robot.h"
+#include "headers/robot.h"
+#include <opencv2/opencv.hpp>
+#include "headers/csupport/MyRio.h"
+#include "headers/myrio/I2C.h"
+#include "headers/myrio/Motor_Controller.h"
+#include "headers/myrio/Utils.h"
 #include <iostream>
-#include "opencv/objdetect.hpp"
-#include "opencv/highgui.hpp"
-#include "opencv/imgproc.hpp"
 #include <math.h>
 #include <fstream>
 
