8a1031cda863840f908a735057332f2fed3d430e
brandonandre <andr0284@algonquinlive.com>
2019-11-25 16:07:59 -0500

    Added image sender code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+/**
+ * Main Robotic Code
+ */
 #include "robot.h"
+#include "ImageSender.h"
+
 #include "opencv2/opencv.hpp"
 #include <iostream>
 #include <math.h>
@@ -7,27 +12,18 @@
 using namespace std;
 using namespace cv;
 
-static void help()
-{
-	cout << "\nThis program demonstrates the use of cv::CascadeClassifier class to detect objects (Face + eyes). You can use Haar or LBP features.\n"
-		"This classifier can recognize many kinds of rigid objects, once the appropriate classifier is trained.\n"
-		"It's most known use is for faces.\n"
-		"Usage:\n"
-		"./facedetect [--cascade=<cascade_path> this is the primary trained classifier such as frontal face]\n"
-		"   [--nested-cascade[=nested_cascade_path this an optional secondary classifier such as eyes]]\n"
-		"   [--scale=<image scale greater or equal to 1, try 1.3 for example>]\n"
-		"   [--try-flip]\n"
-		"   [filename|camera_index]\n\n"
-		"see facedetect.cmd for one call:\n"
-		"./facedetect --cascade=\"data/haarcascades/haarcascade_frontalface_alt.xml\" --nested-cascade=\"data/haarcascades/haarcascade_eye_tree_eyeglasses.xml\" --scale=1.3\n\n"
-		"During execution:\n\tHit any key to quit.\n"
-		"\tUsing OpenCV version " << CV_VERSION << "\n" << endl;
-}
+/* Options */
+#define SEND_IMAGE 1;
 
-void detectAndDraw(Mat& img, CascadeClassifier& cascade,
+/* Function Prototypes */
+void detectAndDraw(
+	Mat& img, 
+	CascadeClassifier& cascade,
 	CascadeClassifier& nestedCascade,
 	CascadeClassifier& mouthCascade,
-	double scale, bool tryflip);
+	double scale, 
+	bool 
+);
 
 string cascadeName;
 string nestedCascadeName;
@@ -35,8 +31,9 @@ string mouthCascadeName;
 long double lowest_right = 0, lowest_left = 0, lowest_mouth = 0;
 long double highest_left = 0, highest_right = 0, highest_mouth = 0;
 
-int main(int argc, const char** argv)
-{
+ImageSender imageSender;
+
+int main(int argc, const char** argv) {
 	VideoCapture capture;
 	Mat frame, image;
 	string inputName;
@@ -44,148 +41,30 @@ int main(int argc, const char** argv)
 	CascadeClassifier cascade, nestedCascade, mouthCascade;
 	double scale;
 
+	// Setup the motor controller.
     begin();
 
-	cv::CommandLineParser parser(argc, argv,
-		"{help h||}"
-		"{cascade|data/haarcascades/haarcascade_frontalface_alt.xml|}"
-		"{nested-cascade|data/haarcascades/haarcascade_eye_tree_eyeglasses.xml|}"
-		"{scale|1|}{try-flip||}{@filename||}"
-	);
-	if (parser.has("help"))
-	{
-		help();
-		return 0;
-	}
-	cascadeName = parser.get<string>("cascade");
-	nestedCascadeName = parser.get<string>("nested-cascade");
-	mouthCascadeName = "data/haarcascades/haarcascade_smile.xml";
-	scale = parser.get<double>("scale");
-	if (scale < 1)
-		scale = 1;
-	tryflip = parser.has("try-flip");
-	inputName = parser.get<string>("@filename");
-	if (!parser.check())
-	{
-		parser.printErrors();
-		return 0;
-	}
-	if (!nestedCascade.load(samples::findFileOrKeep(nestedCascadeName)))
-		cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
-	if (!mouthCascade.load(samples::findFileOrKeep(mouthCascadeName)))
-		cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
-	if (!cascade.load(samples::findFile(cascadeName)))
-	{
-		cerr << "ERROR: Could not load classifier cascade" << endl;
-		help();
-		return -1;
-	}
-	if (inputName.empty() || (isdigit(inputName[0]) && inputName.size() == 1))
-	{
-		int camera = inputName.empty() ? 0 : inputName[0] - '0';
-		if (!capture.open(camera))
-		{
-			cout << "Capture from camera #" << camera << " didn't work" << endl;
-			return 1;
-		}
-	}
-	else if (!inputName.empty())
-	{
-		image = imread(samples::findFileOrKeep(inputName), IMREAD_COLOR);
-		if (image.empty())
-		{
-			if (!capture.open(samples::findFileOrKeep(inputName)))
-			{
-				cout << "Could not read " << inputName << endl;
-				return 1;
-			}
-		}
-	}
-	else
-	{
-		image = imread(samples::findFile("lena.jpg"), IMREAD_COLOR);
-		if (image.empty())
-		{
-			cout << "Couldn't read lena.jpg" << endl;
-			return 1;
-		}
-	}
-
-	if (capture.isOpened())
-	{
+	// Start capturing video from the webcam.
+	if (capture.isOpened()) {
 		cout << "Video capturing has been started ..." << endl;
 
-		for (;;)
-		{
+		// Setup the image sender.
+		if(imageSender.init() < 0) {
+			return 1;
+		} else {
+			cout << "Image sender could not be setup. Contining without it.";
+		}
+
+		while(true) {
 			capture >> frame;
 			if (frame.empty())
 				break;
 
 			Mat frame1 = frame.clone();
 			detectAndDraw(frame1, cascade, nestedCascade, mouthCascade, scale, tryflip);
-
-			char c = (char)waitKey(10);
-			if (c == 27 || c == 'q' || c == 'Q') {
-#ifdef LEARNING
-				FILE* outputFile = fopen("C:\\TEMP\\output.txt", "w");
-				fprintf(outputFile, "Left lowest: %lf highest: %lf\n", lowest_left, highest_left);
-				fprintf(outputFile, "Right lowest: %lf highest: %lf\n", lowest_right, highest_right);
-				fprintf(outputFile, "Mouth lowest: %lf highest: %lf\n", lowest_mouth, highest_mouth);
-				fclose(outputFile);
-#endif
-				break;
-			}
 		}
 	}
-	else
-	{
-		cout << "Detecting face(s) in " << inputName << endl;
-		if (!image.empty())
-		{
-			detectAndDraw(image, cascade, nestedCascade, mouthCascade, scale, tryflip);
-			waitKey(0);
-		}
-		else if (!inputName.empty())
-		{
-			/* assume it is a text file containing the
-			list of the image filenames to be processed - one per line */
-			FILE* f = fopen(inputName.c_str(), "rt");
-			if (f)
-			{
-				char buf[1000 + 1];
-				while (fgets(buf, 1000, f))
-				{
-					int len = (int)strlen(buf);
-					while (len > 0 && isspace(buf[len - 1]))
-						len--;
-					buf[len] = '\0';
-					cout << "file " << buf << endl;
-					image = imread(buf, 1);
-					if (!image.empty())
-					{
-						detectAndDraw(image, cascade, nestedCascade, mouthCascade, scale, tryflip);
-						char c = (char)waitKey(0);
-						if (c == 27 || c == 'q' || c == 'Q') {
-#ifdef LEARNING
-							FILE* outputFile = fopen("C:\\TEMP\\output.txt", "w");
-							fprintf(outputFile, "Left lowest: %lf highest: %lf\n", lowest_left, highest_left);
-							fprintf(outputFile, "Right lowest: %lf highest: %lf\n", lowest_right, highest_right);
-							fprintf(outputFile, "Mouth lowest: %lf highest: %lf\n", lowest_mouth, highest_mouth);
-							fclose(outputFile);
-#endif
-							break;
-						}
-					}
-					else
-					{
-						cerr << "Aw snap, couldn't read image " << buf << endl;
-					}
-				}
-				fclose(f);
-			}
-		}
-	}
-
+	
 	return 0;
 }
 
@@ -322,50 +201,6 @@ void detectAndDraw(Mat& img, CascadeClassifier& cascade,
 			long double right_distance = distance_eyes / right_to_center;
 			long double mouth_distance = distance_eyes / mouth_to_center;
 
-#ifdef LEARNING
-			if (lowest_left == 0) {
-				lowest_left = left_distance;
-			}
-			else if (lowest_left > left_distance) {
-				lowest_left = left_distance;
-			}
-
-			if (lowest_right == 0) {
-				lowest_right = right_distance;
-			}
-			else if (lowest_right > right_distance) {
-				lowest_right = right_distance;
-			}
-
-			if (lowest_mouth == 0) {
-				lowest_mouth = mouth_distance;
-			}
-			else if (lowest_mouth > mouth_distance) {
-				lowest_mouth = mouth_distance;
-			}
-
-			if (highest_left == 0) {
-				highest_left = left_distance;
-			}
-			else if (highest_left < left_distance) {
-				highest_left = left_distance;
-			}
-
-			if (highest_right == 0) {
-				highest_right = right_distance;
-			}
-			else if (highest_right < right_distance) {
-				highest_right = right_distance;
-			}
-
-			if (highest_mouth == 0) {
-				highest_mouth = mouth_distance;
-			}
-			else if (highest_mouth < mouth_distance) {
-				highest_mouth = mouth_distance;
-			}
-#endif
-
 			if ((left_distance >= 1.629754 && left_distance <= 1.761912) && (right_distance >= 1.586607 && right_distance <= 1.801996) && (mouth_distance >= 1.352993 && mouth_distance <= 1.584232)) {
 				printf("HI MICHAEL!\n");
                 moveDistance(300, SPEED, FORWARDS);
@@ -386,5 +221,11 @@ void detectAndDraw(Mat& img, CascadeClassifier& cascade,
 			circle(img, center, radius, color, 3, 8, 0);
 		}
 	}
+
+	/* Image Sending */
+	#if SEND_IMAGE
+	    imageSender.send(&img);
+	#endif
+
 	imshow("result", img);
 }
